5b0cf94c72df523457e51d92378ea1780ded1734
Knight-Thomas <thomasmilesknight@icloud.com>
2024-02-26 10:57:43 +0000

    changing names to allow easier user vison

---
+++ LogInApp.py
@@ -0,0 +1,25 @@
+#import required modules
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import sys
+
+#Eacvh window will need its own class
+class Ui(QtWidgets.QMainWindow):
+    '''constructor'''
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('Login.ui', self) #loads ui we just created for this class
+
+        #add button functionality
+
+    
+        #display the ui
+        self.show()
+
+#write main applicatiion which creates class instance
+
+def mainApplication():
+    '''main app which instantiates the classes'''
+    app=QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
+mainApplication()
